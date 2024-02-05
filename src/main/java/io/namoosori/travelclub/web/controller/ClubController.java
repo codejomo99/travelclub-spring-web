f0e833df218aa5f779f51a087225d079eaf986b5
Jong-Min <codejomo99@gmail.com>
2024-02-05 17:40:04 +0900

    [GetMapping]findById 구현

--- src/main/java/io/namoosori/travelclub/web/controller/ClubController.java
+++ src/main/java/io/namoosori/travelclub/web/controller/ClubController.java
@@ -1,12 +1,12 @@
 package io.namoosori.travelclub.web.controller;
 
 
+import io.namoosori.travelclub.web.aggregate.club.TravelClub;
 import io.namoosori.travelclub.web.service.ClubService;
 import io.namoosori.travelclub.web.service.sdo.TravelClubCdo;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class ClubController {
@@ -20,6 +20,16 @@ public class ClubController {
     @PostMapping("/club") //localhost:8090//club
     public String register(@RequestBody TravelClubCdo travelClubCdo){
         return clubService.registerClub(travelClubCdo);
+    }
+
+    @GetMapping("/club/all")
+    public List<TravelClub> findAll(){
+        return clubService.findAll();
+    }
+
+    @GetMapping("/club/{clubId}")
+    public TravelClub find(@PathVariable String clubId){
+        return clubService.findClubById(clubId);
     }
 
 }
