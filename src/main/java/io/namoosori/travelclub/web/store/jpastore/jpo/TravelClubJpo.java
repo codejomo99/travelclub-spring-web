1682f32a621bf036080f280e551277529bb961ec
Jong-Min <codejomo99@gmail.com>
2024-02-06 16:36:41 +0900

    [Spring Data Jpa] ClubJpa 제작

--- src/main/java/io/namoosori/travelclub/web/store/jpastore/jpo/TravelClubJpo.java
+++ src/main/java/io/namoosori/travelclub/web/store/jpastore/jpo/TravelClubJpo.java
@@ -1,16 +1,20 @@
 package io.namoosori.travelclub.web.store.jpastore.jpo;
 
+import io.namoosori.travelclub.web.aggregate.club.TravelClub;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.beans.BeanUtils;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
+@Table(name="TRAVEL_CLUB")
 public class TravelClubJpo {
 
     @Id
@@ -19,5 +23,22 @@ public class TravelClubJpo {
     private String intro;
     private long foundationTime;
 
+    public TravelClubJpo(TravelClub travelClub){
+        BeanUtils.copyProperties(travelClub, this);
+        // this.id = travelClub.getId();
+        // . . . . . . . . . 같은역할
+    }
+
+    public TravelClub toDomain(){
+        TravelClub travelClub = new TravelClub(this.name, this.intro);
+        travelClub.setId(this.id);
+        travelClub.setFoundationTime(this.foundationTime);
+        return travelClub;
+    }
 
 }
+
+
+
+
+
